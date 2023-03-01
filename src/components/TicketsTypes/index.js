6f6cdb5e4249750711889c4712ce218b8ffa7c3a
isabellexvr <isabellexavier59@gmail.com>
2023-03-01 09:29:56 -0300

    feat: shows message when user is not enrolled yet

--- src/components/TicketsTypes/index.js
+++ src/components/TicketsTypes/index.js
@@ -1,22 +1,19 @@
 import useGetEnrollment from '../../hooks/api/useGetEnrollment';
+import { NotRegisteredMessage } from './NotRegisteredMessage';
+import { useState } from 'react';
 
 export default function TicketsTypes() {
-  const {
-    getEnrollmentLoading,
-    getEnrollmentError,
-    data
-  } = useGetEnrollment();
-  console.log(data);
+  const { getEnrollmentLoading, getEnrollmentError, data } = useGetEnrollment();
+  const [presentState, setPresentState] = useState(false);
   return (
     <>
-      {!data ? (
-        <>
-          aaa porraa
-        </>
+      {data ? (
+        <NotRegisteredMessage>
+          <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>
+        </NotRegisteredMessage>
       ) : (
-        <>
-          olha os dados ai
-        </>)}
+        <>olha os dados ai</>
+      )}
     </>
   );
 }
