48d67faf938d5306184006e5c9a35a28bcae48d9
Melissa <melmihomel@gmail.com>
2023-03-09 18:52:33 -0300

    feat: add restrictions to select activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,20 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import ActivitiesSelection from '../../../components/Activities';
+import useGetTicket from '../../../hooks/api/useGetTicket';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { getTicket } = useGetTicket();
+  const [ticket, setTicket] = useState({});
+
+  useEffect(async() => {
+    try {
+      const newTicket = await getTicket();
+      setTicket(newTicket);
+    } catch (err) {
+      toast('Primeiro escolha seu ticket!');
+    }
+  }, [ticket]);
+
+  return <ActivitiesSelection ticket={ticket} />;
 }
